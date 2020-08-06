1fd569ad1cb8e29341f57054ea8e7a532183ad12
avaska <neoquro12@gmail.com>
2020-08-06 10:17:32 +0900

    200806 수업_1

--- SpringTest/src/test/java/com/itwillbs/test/MysqlConnectionTest.java
+++ SpringTest/src/test/java/com/itwillbs/test/MysqlConnectionTest.java
@@ -2,6 +2,7 @@ package com.itwillbs.test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import org.junit.Test;
 
@@ -24,18 +25,54 @@ public class MysqlConnectionTest {
 	// 테스트 동작을 메서드로 구현 후 실행
 	
 	// 디비연결 테스트
-	//@Test 어노테이션 : JUnit 실행
+	//@Test 어노테이션 : JUnit 실행 -> 서버에서 작동하는것처럼 테스트 가능	
 	@Test
-	public void testConnection() throws Exception {
+	public void testConnection() {		
+		try {
+			// 드라이버 로드
+			Class.forName(DRIVER);
+			// 디비연결
+			Connection con = DriverManager.getConnection(DBURL, DBID, DBPW);			
+
+			System.out.println("디비 연결 객체 : " + con);
+			
+			con.close();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 		// ->try-with구문 사용
-		
-		//드라이버 로드
-		Class.forName(DRIVER);
-		//디비 연결
-		Connection con = 
-				DriverManager.getConnection(DBURL, DBID, DBPW);
-		
-		System.out.println("디비 연결 객체 : " + con);
+		// finally 블럭 없이 자동으로 자원 해제 처리 가능하도록 만들어진 구문
+		// jdk7 이상 사용 가능(jdk7 미만 버전에서는 컴파일 에러)
+		/*
+		 * try(AutoCloseable 객체) {
+		 * 
+		 * }catch (Exception e) {
+		 * 
+		 * }
+		 */
+		// 드라이버 로드
+		try {
+			Class.forName(DRIVER);
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		// 디비연결
+		try (Connection con = DriverManager.getConnection(DBURL, DBID, DBPW);) {
+			// Connection : AutoCloseable 인터페이스를 상속
+			// try( ) 안에 넣어주면 finally블럭 없이 자동으로 자원해제 처리 가능
+			
+			// Hierarchy 보기 : 
+			// Connection에 마우스 대고 F4키 누르면 좌측에 Type Hierarchy 생김
+			// 2번째 버튼인 Supertype hierarchy로 보기
+
+			System.out.println("디비 연결 객체 : " + con);
+			
+		}  catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
